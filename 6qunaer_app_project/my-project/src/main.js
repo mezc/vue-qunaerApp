c1618279aa59a6233f3686130c96a7aa9b76975e
C Zhang <Czhang@CdeMacBook-Pro.local>
2019-04-14 17:00:04 +0800

    project init

--- 6qunaer_app_project/my-project/src/main.js
+++ 6qunaer_app_project/my-project/src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import "./assets/styles/reset.css"
+// 解决多倍屏中1像素边框被显示为多像素的问题
+import "./assets/styles/border.css"
+// 解决移动端某些机型在执行click事件时，延时300ms
+import fastClick from "fastclick"
+// import "./assets/styles/iconfont.css"
 
+
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: { App : App}, 
   template: '<App/>'
 })
