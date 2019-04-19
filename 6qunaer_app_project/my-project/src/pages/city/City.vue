4d3bb60f843385541b94be9a9b5dd1cc76e01d4b
C Zhang <Czhang@CdeMacBook-Pro.local>
2019-04-19 11:21:58 +0800

    add search to city

--- 6qunaer_app_project/my-project/src/pages/city/City.vue
+++ 6qunaer_app_project/my-project/src/pages/city/City.vue
@@ -1,18 +1,25 @@
 <template>
     <!-- <div>city</div> -->
-    <city-header></city-header>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
+    
 </template>
 
 <script>
 import CityHeader from "./components/CityHeader.vue"
+import CitySearch from "./components/CitySearch.vue"
+
 export default {
     name:"City",
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+    
 </style>
