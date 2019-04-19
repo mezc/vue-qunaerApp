5b061e6d931afabf957adc9781aab1fed1bd4d5c
C Zhang <Czhang@CdeMacBook-Pro.local>
2019-04-19 10:55:41 +0800

    add city-router

---
+++ 6qunaer_app_project/my-project/src/pages/city/components/CityHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+            <span class="iconfont back-icon" >&#xe624;</span>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import "~styles/varibles.styl"
+    .header
+        position relative
+        overflow hidden
+        height .86rem
+        line-height .86rem
+        text-align center
+        color #fff
+        background-color $bgColor
+        .back-icon
+            position absolute
+            width .64rem
+            text-align center
+            font-size .4rem
+            left 0
+            top 0
+            color #fff
+            
+
+
+</style>
+
